f7b194f7219d9e7ff520a60b003950eb46dc9a55
Adam Dawkins <adamdawkins@gmail.com>
2016-02-17 23:59:16 +0000

    If a splint was made of flint, would my code lint?

--- client/modules/core/components/tests/userlist.js
+++ client/modules/core/components/tests/userlist.js
@@ -1,39 +1,8 @@
 const {describe, it} = global;
-import {expect} from 'chai';
-import {shallow} from 'enzyme';
-import UserList from '../userlist.jsx';
+// import {expect} from 'chai';
+// import {shallow} from 'enzyme';
+// import UserList from '../userlist.jsx';
 
 describe('core.components.userlist', () => {
-  const users = [
-    {
-      _id: 'naoethunaoth',
-      username: 'adamdawkins',
-      profile: {
-        name: {
-          title: 'Mr',
-          first: 'Adam',
-          last: 'Dawkins'
-        },
-        picture: {
-          thumbnail: 'https://randomuser.me/api/portraits/thumb/men/76.jpg'
-        }
-      }
-    },
-    {
-      _id: 'asnoetuhanoteuh',
-      username: 'johntnorris',
-      profile: {
-        name: {
-          title: 'Mr',
-          first: 'John',
-          last: 'Norris'
-        },
-        picture: {
-          thumbnail: 'https://randomuser.me/api/portraits/thumb/men/76.jpg'
-        }
-      }
-    }
-  ];
-
   it('should have some tests');
 });
