86e4eb6b2de2d4745b9cfddfe2742f02609fcd13
Adam Dawkins <adamdawkins@gmail.com>
2016-02-17 14:24:50 +0000

    Sort tweets on client side

--- client/modules/core/containers/feed.js
+++ client/modules/core/containers/feed.js
@@ -4,7 +4,7 @@ import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
 export const composer = ({context}, onData) => {
   const {Meteor, Collections} = context();
   if (Meteor.subscribe('tweets.all').ready()) {
-    const tweets = Collections.Tweets.find().fetch();
+    const tweets = Collections.Tweets.find({}, { sort:{createdAt: -1}}).fetch();
     onData(null, {tweets});
   }
 };
