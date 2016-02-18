037e4520dbe33a0237af28afadeba72cd252792c
Adam Dawkins <adamdawkins@gmail.com>
2016-02-18 12:54:28 +0000

    Add login route

--- client/modules/_accounts/routes.jsx
+++ client/modules/_accounts/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'; import {mount} from 'react-mounter';
 import {Layout} from '/client/configs/theme.jsx';
 import Register from './containers/register';
+import Login from './containers/login';
 
 export default function (injectDeps, {FlowRouter}) {
   const context = injectDeps(Layout);
@@ -10,6 +11,15 @@ export default function (injectDeps, {FlowRouter}) {
     action() {
       mount(context, {
         content: () => (<Register/>)
+      });
+    }
+  });
+
+  FlowRouter.route('/login', {
+    name: 'accounts.login',
+    action() {
+      mount(context, {
+        content: () => (<Login/>)
       });
     }
   });
