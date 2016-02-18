3d4c684d228f180dc727c65d5fdc9845426e833c
Adam Dawkins <adamdawkins@gmail.com>
2016-02-18 11:20:08 +0000

    Add login / register links to layout

--- client/modules/_theme/components/main_layout.jsx
+++ client/modules/_theme/components/main_layout.jsx
@@ -4,6 +4,7 @@ const Layout = ({content = () => null }) => (
   <div>
     <header>
       <h1>Mantra Twitter</h1>
+      <a href="/register">Sign Up</a> | <a href="/login">Login</a>
     </header>
     <main>
       {content()}
